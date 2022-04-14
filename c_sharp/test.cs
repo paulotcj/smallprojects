42e9e58aa4b06f1c7f880d5faba0adc6a5525313
Paulo Costa <paulotcj@gmail.com>
2022-04-14 09:18:22 -0400

    .

---
+++ c_sharp/test.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Scripts
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("This is the miracle");
+        }
+    }
+}
