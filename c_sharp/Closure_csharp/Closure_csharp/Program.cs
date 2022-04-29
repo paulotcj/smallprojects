b549c71f105ebe31973c0d95de43eb72303d5083
Paulo Costa <paulotcj@gmail.com>
2022-04-28 20:20:28 -0400

    .

---
+++ c_sharp/Closure_csharp/Closure_csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+var abc = ClosureTest();
+
+abc();
+abc();
+abc();
+
+
+
+static Func<int> ClosureTest()
+{
+    int foo = 0;
+
+    Func<int> Bar = () => 
+    { 
+        foo++;
+        Console.WriteLine($"Foo: {foo}");
+        return foo; 
+    };
+
+    return Bar;
+}
