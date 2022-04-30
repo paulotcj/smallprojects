4398f48053a81a9b2ecf90dd88e96b6f74db45d1
Paulo <paulotcj@gmail.com>
2022-04-30 19:14:09 -0400

    .

---
+++ c_sharp/HashTables/FirstRecurringCharacters/FirstRecurringCharacters/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+Action<string> print = Console.WriteLine;
+
+print("Hello, World!");
+int[] arr;
+
+arr = new int[] {  };
+RecurringCharacters.FindFirstOccurence(arr);
+
+arr = new int[] { 1 };
+RecurringCharacters.FindFirstOccurence(arr);
+
+arr = new int[] { 2,1 };
+RecurringCharacters.FindFirstOccurence(arr);
+
+arr = new int[] { 2,2 };
+RecurringCharacters.FindFirstOccurence(arr);
+
+arr = new int[] {1,2,1 };
+RecurringCharacters.FindFirstOccurence(arr);
+
+
+
+print("##############################");
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+arr = new int[] { 2, 5, 1, 2, 3, 5, 1, 2, 4 };
+RecurringCharacters.FindFirstOccurence(arr);
+print("-----------");
+
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+arr = new int[] { 2, 1, 1, 2, 3, 5, 1, 2, 4 };
+RecurringCharacters.FindFirstOccurence(arr);
+print("This result is not what is expected according to the guidelines");
+print("-----------");
+
+print("##############################");
+
+
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+arr = new int[] { 2, 5, 1, 2, 3, 5, 1, 2, 4 };
+RecurringCharacters.FindFirstOccurence2(arr);
+print("-----------");
+
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+arr = new int[] { 2, 1, 1, 2, 3, 5, 1, 2, 4 };
+RecurringCharacters.FindFirstOccurence2(arr);
+print("-----------");
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+arr = new int[] { 2, 3, 4, 5 };
+RecurringCharacters.FindFirstOccurence2(arr);
+print("-----------");
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+
+arr = new int[] { 2, 5, 5, 2, 3, 5, 1, 2, 4 };
+RecurringCharacters.FindFirstOccurence2(arr);
+print("-----------");
+
+
+
+//-------------------------------------
+
+public static class RecurringCharacters
+{
+    //this will work if we consider valid the values alongside the array
+    public static int? FindFirstOccurence(int[] arr)
+    {
+        for(int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i+1; j < arr.Length; j++)
+            {
+                if (arr[i] == arr[j])
+                { Console.WriteLine($"Found {arr[j]}"); return arr[j]; }
+            }
+
+        }
+
+        Console.WriteLine($"(Not Found)");
+        return null;
+    }
+
+    
+    public static int? FindFirstOccurence2(int[] arr)
+    {
+
+        var dic = new Dictionary<int, int>();
+        foreach (var i in arr)
+        {
+            if (dic.ContainsKey(i) == true)
+            {
+                dic[i] = dic[i]++;
+                Console.WriteLine($"Found {i}");
+                return i;
+            }
+
+            dic.Add(i, 1);
+            
+        }
+
+
+        Console.WriteLine($"(Not Found)");
+        return null;
+    }
+}
+
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+
+//function firstRecurringCharacter(input)
+//}
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
---
+++ c_sharp/HashTables/HashTables/HashTables/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+Action<string> print = Console.WriteLine;
+
+print("Hello, World!");
+
+var hashObj = new MyHashTable(5);
+
+print($"Value of 'aa' : {hashObj.Hash("aa")}");
+print($"Value of 'ab' : {hashObj.Hash("ab")}");
+
+hashObj.Insert("a");
+hashObj.Insert("b"); //provoking collision
+hashObj.Insert("f"); //provoking collision
+hashObj.Insert("z"); //provoking collision
+hashObj.Insert("g"); //provoking collision
+hashObj.Insert("h"); //provoking collision
+hashObj.Insert("t"); //provoking collision
+
+hashObj.PrintHashTable();
+
+hashObj.Insert("Mike");
+hashObj.Insert("Lia");
+hashObj.Insert("Mel");
+hashObj.Insert("John");
+hashObj.Insert("Marlene");
+hashObj.Insert("Pfutzenreuter"); // :P haha
+
+hashObj.PrintHashTable();
+
+print("------------------------------------");
+print($"trying to find: Mike -> {hashObj.Get("Mike")}");
+print($"trying to find: z -> {hashObj.Get("z")}");
+print($"trying to find: Bob -> {hashObj.Get("Bob")}");
+
+
+
+
+public class MyHashTable
+{
+    //we need to have an array
+    //  of lists - and these lists are lists of tempdata
+
+    private List<string>[] data;
+    public MyHashTable(int size)
+    {
+        data = new List<string>[size];
+    }
+
+    public int Hash(string value)
+    {
+        var returnObj = 0;
+        for (var i = 0; i < value.Length; i++)
+        {
+            returnObj = (returnObj + (int)value[i] * i);
+        }
+        returnObj = returnObj % this.data.Length;
+
+        return returnObj;
+    }
+
+    public void Insert(string value)
+    {
+        var hash = Hash(value);
+        if (this.data[hash] == null) { this.data[hash] = new List<string>(); }
+        this.data[hash].Add( value);
+    }
+
+
+    public string? Get(string value)
+    {
+        var hash = Hash(value);
+        var itemArray = this.data[hash];
+        foreach (var tempObj in itemArray)
+        {
+            if (value.CompareTo(tempObj) == 0)
+            {
+                //Console.WriteLine($"Item found : { tempObj }");
+                return tempObj;
+            }
+        }
+        return "(Not Found)";
+    }
+
+    public void PrintHashTable()
+    {
+        Console.WriteLine("-----------------------------------------\nPrintHashTable");
+        for (int i = 0; i < this.data.Length; i++)
+        {
+            Console.WriteLine($"Key{i}");
+            foreach (var item in this.data[i]??new List<string>())
+            {
+                Console.WriteLine($"    Value:{item}");
+            }
+        }
+    }
+}
+
+//public class TempData
+//{
+//    public int objKey = 0;
+//    public string objValue;
+//    public TempData(int value, string key)
+//    {
+//        objKey = value;
+//        objValue = key;
+//    }
+//}
