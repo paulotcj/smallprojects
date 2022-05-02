c2438b0347e80e7b6eb7750b96ae441e50a81559
Paulo Costa <paulotcj@gmail.com>
2022-05-01 21:00:42 -0400

    .

---
+++ c_sharp/study_delete/Stack_using_Array/Stack_using_Array/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+Action<string> print = Console.WriteLine;
+
+print("Hello, World!");
+
+var myStack = new MyStack(10);
+myStack.Push(2);
+print(myStack.Peek().ToString());
+print("##############################");
+myStack.Push(4);
+myStack.Push(6);
+myStack.Push(8);
+print(myStack.Peek().ToString());
+print("##############################");
+print("Info: 2 was the first added and 8 was the last added");
+print("##############################");
+myStack.PrintList();
+
+print("##############################");
+print($"Stack Pop: {myStack.Pop()}");
+
+myStack.PrintList();
+print("##############################");
+print($"Stack Pop: {myStack.Pop()}");
+
+myStack.PrintList();
+
+print("##############################");
+print("Multiple Pop operations coming up...");
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+print("Stack should be empty and no errors should've occurred.");
+myStack.PrintList();
+
+
+
+
+
+//------------------------------------------
+public class MyStack
+{
+    //private List<int> _list = new List<int>();
+    private int?[] _array;
+    private int _lastAddedIndex = -1;
+    public MyStack(int arraySize)
+    {
+        _array = new int?[arraySize];
+    }
+
+    public void Push(int value)
+    {
+        if (_lastAddedIndex + 1 >= _array.Length) { return; }
+        _array[++_lastAddedIndex] = value;
+    }
+
+    public int? Peek()
+    {
+        if (_lastAddedIndex < 0) { return null; }
+        return _array[_lastAddedIndex];
+    }
+
+    public int? Pop()
+    {
+        var returnObj = Peek();
+        if (returnObj != null) 
+        {
+            _array[_lastAddedIndex] = null;
+            _lastAddedIndex--;
+        }
+
+        return returnObj;
+    }
+
+    public void PrintList()
+    {
+        Console.WriteLine("--------------------------\nPrint Stack/Array:");
+        foreach (var i in _array)
+        {
+            if (i == null) { continue; }
+            Console.WriteLine($"Value : {i}");
+        }
+    }
+}
---
+++ c_sharp/study_delete/Stack_using_LinkedList/Stack_using_LinkedList/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+Action<string> print = Console.WriteLine;
+print("Hello, World!");
+
+var myStack = new MyStack(2);
+print(myStack.Peek().ToString());
+print("##############################");
+myStack.Push(4);
+myStack.Push(6);
+myStack.Push(8);
+print(myStack.Peek().ToString());
+print("##############################");
+print("Info: 2 was the first added and 8 was the last added");
+print("##############################");
+myStack.PrintList();
+
+print("##############################");
+print($"Stack Pop: {myStack.Pop()}");
+
+myStack.PrintList();
+print("##############################");
+print($"Stack Pop: {myStack.Pop()}");
+
+myStack.PrintList();
+
+print("##############################");
+print("Multiple Pop operations coming up...");
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+myStack.Pop();
+print("Stack should be empty and no errors should've occurred.");
+myStack.PrintList();
+
+
+
+
+//------------------------------------
+
+public class MyStack
+{
+    private List<int> _list =  new List<int>();
+    public MyStack(int value) 
+    {
+        _list.Add(value);
+    }
+
+    public void Push(int value) 
+    {
+        _list.Add(value);
+    }
+
+    public int Peek()
+    {
+        var lastAdded = _list[_list.Count -1];
+        return lastAdded;
+    }
+
+    public int? Pop()
+    {
+        var lastIndex = _list.Count - 1;
+        if (lastIndex < 0) { return null; }
+        var returnObj = _list[lastIndex];
+
+        _list.RemoveAt(lastIndex);
+
+        return returnObj;
+    }
+
+    public void PrintList()
+    {
+        Console.WriteLine("--------------------------\nPrint Stack/List:");
+        foreach (var i in _list)
+        {
+            Console.WriteLine($"Value : {i}");
+        }
+    }
+}
