08eafde1fd1fa70be101d36dfc49595689ebbaf7
Paulo Costa <paulotcj@gmail.com>
2022-05-13 15:12:33 -0400

    .

---
+++ c_sharp/Algorithms/Search/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125, 127, 129, 131, 133, 135, 137, 139, 141, 143, 145, 147, 149, 151, 153, 155, 157, 159, 161, 163, 165, 167, 169, 171, 173, 175, 177, 179, 181, 183, 185, 187, 189, 191, 193, 195, 197, 199, 201, 203, 205, 207, 209, 211, 213, 215, 217, 219, 221, 223, 225, 227, 229, 231, 233, 235, 237, 239, 241, 243, 245, 247, 249, 251, 253, 255, 257, 259, 261, 263, 265, 267, 269, 271, 273, 275, 277, 279, 281, 283, 285, 287, 289, 291, 293, 295, 297, 299, 301, 303, 305, 307, 309, 311, 313, 315, 317, 319, 321, 323, 325, 327, 329, 331, 333, 335, 337, 339, 341, 343, 345, 347, 349, 351, 353, 355, 357, 359, 361, 363, 365, 367, 369, 371, 373, 375, 377, 379, 381, 383, 385, 387, 389, 391, 393, 395, 397, 399, 401, 403, 405, 407, 409, 411, 413, 415, 417, 419, 421, 423, 425, 427, 429, 431, 433, 435, 437, 439, 441, 443, 445, 447, 449, 451, 453, 455, 457, 459, 461, 463, 465, 467, 469, 471, 473, 475, 477, 479, 481, 483, 485, 487, 489, 491, 493, 495, 497, 499, 501, 503, 505, 507, 509, 511, 513, 515, 517, 519, 521, 523, 525, 527, 529, 531, 533, 535, 537, 539, 541, 543, 545, 547, 549, 551, 553, 555, 557, 559, 561, 563, 565, 567, 569, 571, 573, 575, 577, 579, 581, 583, 585, 587, 589, 591, 593, 595, 597, 599, 601, 603, 605, 607, 609, 611, 613, 615, 617, 619, 621, 623, 625, 627, 629, 631, 633, 635, 637, 639, 641, 643, 645, 647, 649, 651, 653, 655, 657, 659, 661, 663, 665, 667, 669, 671, 673, 675, 677, 679, 681, 683, 685, 687, 689, 691, 693, 695, 697, 699, 701, 703, 705, 707, 709, 711, 713, 715, 717, 719, 721, 723, 725, 727, 729, 731, 733, 735, 737, 739, 741, 743, 745, 747, 749, 751, 753, 755, 757, 759, 761, 763, 765, 767, 769, 771, 773, 775, 777, 779, 781, 783, 785, 787, 789, 791, 793, 795, 797, 799, 801, 803, 805, 807, 809, 811, 813, 815, 817, 819, 821, 823, 825, 827, 829, 831, 833, 835, 837, 839, 841, 843, 845, 847, 849, 851, 853, 855, 857, 859, 861, 863, 865, 867, 869, 871, 873, 875, 877, 879, 881, 883, 885, 887, 889, 891, 893, 895, 897, 899, 901, 903, 905, 907, 909, 911, 913, 915, 917, 919, 921, 923, 925, 927, 929, 931, 933, 935, 937, 939, 941, 943, 945, 947, 949, 951, 953, 955, 957, 959, 961, 963, 965, 967, 969, 971, 973, 975, 977, 979, 981, 983, 985, 987, 989, 991, 993, 995, 997, 999, 1001, 1003, 1005, 1007, 1009, 1011, 1013, 1015, 1017, 1019, 1021, 1023, 1025, 1027, 1029, 1031, 1033, 1035, 1037, 1039, 1041, 1043, 1045, 1047, 1049, 1051, 1053, 1055, 1057, 1059, 1061, 1063, 1065, 1067, 1069, 1071, 1073, 1075, 1077, 1079, 1081, 1083, 1085, 1087, 1089, 1091, 1093, 1095, 1097, 1099, 1101, 1103, 1105 };
+
+Search.Linear(arr, 1053);
+Search.Linear(arr, 1052);
+Console.WriteLine("################");
+
+int[] arr2 = new int[]{ 1,3,5,7,9 };
+Search.Binary(arr2, 1);
+Console.WriteLine("################");
+Search.Binary(arr2, 3);
+Console.WriteLine("################");
+Search.Binary(arr2, 5);
+Console.WriteLine("################");
+Search.Binary(arr2, 7);
+Console.WriteLine("################");
+Search.Binary(arr2, 9);
+Console.WriteLine("################");
+Search.Binary(arr2, 1);
+Console.WriteLine("################");
+Search.Binary(arr2, 2);
+Console.WriteLine("################");
+Search.Binary(arr, 1053);
+Search.Binary(arr, 1052);
+
+//----------------------
+public static class Search
+{
+    public static void Binary(int[] arr, int itemToFind)
+    {
+        
+        int lower_idx = 0;
+        int upper_idx = arr.Length - 1;
+        int middle_idx = 0;
+        int count = 0;
+
+        while (true) 
+        {
+            //suppose we have lo(4) + up(11), then -> ((11-4) + 1) / 2 = (7 + 1) / 2 = 8 / 2 = 4
+            // 4 by itself only tells us how many steps we need to move. Now we need to add the index of the
+            // lower index in order to make any sense (4) + lo(4) = 8
+            middle_idx = ((upper_idx - lower_idx + 1) / 2)+lower_idx;
+            int middle_val = arr[middle_idx];
+
+            if (middle_val == itemToFind)
+            {
+                Console.WriteLine($"Found! Arr[{middle_idx}] = {itemToFind}");
+                Console.WriteLine($"    Steps taken:{count}");
+
+                return;
+            }
+            else if (lower_idx == upper_idx) 
+            {
+                Console.WriteLine("Item is not present in the array (lower_idx == upper_idx)");
+                break;
+            }
+            else if (itemToFind < middle_val)
+            {
+                upper_idx = middle_idx - 1;
+            }
+            else // itemToFind > middle_val
+            {
+                lower_idx = middle_idx + 1;
+            }
+
+            //---------------
+
+            count++; //let's try to prevent an infinite loop;
+            if (count == arr.Length)
+            {
+                Console.WriteLine("Safeguard condition, preventing infinite loop");
+                break;
+            }
+
+
+        }
+
+        Console.WriteLine($"Not Found! {itemToFind}");
+        Console.WriteLine($"    Steps taken:{count}");
+        return;
+
+    }
+
+
+    public static void Linear(int[] arr , int itemToFind)
+    {
+        for(int i = 0; i < arr.Length; i++)
+        {
+            int item = arr[i];
+            if (item == itemToFind) 
+            { 
+                Console.WriteLine($"Found! Arr[{i}] = {item}");
+                Console.WriteLine($"    Steps taken:{i}");
+
+                return;
+            }
+        }
+
+        Console.WriteLine($"Not Found! {itemToFind}");
+        Console.WriteLine($"    Steps taken:{arr.Length}");
+
+    }
+
+
+
+}
---
+++ c_sharp/Algorithms/Search/Tree_BFS/Tree_BFS/Program.cs
@@ -0,0 +1,415 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Action<string> print = Console.WriteLine;
+
+print("Hello, World!");
+
+var myTree = new MyBinaryTree();
+myTree.Insert(9);
+myTree.PrintTree();
+print("##############################");
+myTree.Insert(4);
+myTree.Insert(6);
+myTree.PrintTree();
+myTree.Insert(20);
+myTree.Insert(170);
+myTree.Insert(15);
+myTree.Insert(1);
+myTree.PrintTree();
+//     9
+//  4     20
+//1  6  15  170
+
+print("BreadthFirstSearch() below");
+var myList = myTree.BreadthFirstSearch();
+Console.WriteLine($"Printing List. It should be: 9 , 4 , 20 , 1 , 6 , 15 , 170");
+myTree.PrintList(myList);
+
+print("##############################");
+print("BFS() below");
+myList = myTree.My_BFS();
+myTree.PrintList(myList);
+print("##############################");
+print("BFS_Recursive() below");
+myList = myTree.My_BFS_Recursive();
+myTree.PrintList(myList);
+
+//------------------------------
+
+public class MyBinaryTree
+{
+    private Node root = null;
+
+    public List<Node> My_BFS()
+    {
+        List<Node> list = new();
+        Queue<Node> q = new();
+        Node current;
+        q.Enqueue(root);
+
+        while (q.Count > 0)
+        {     
+            current = q.Dequeue();
+            list.Add(current);
+
+            if(current.left != null) { q.Enqueue(current.left); }
+            if (current.right != null) { q.Enqueue(current.right); }
+
+        }
+
+        return list;
+
+    }
+
+
+    public List<Node> My_BFS_Recursive(Queue<Node> q = null , List<Node> list = null)
+    {
+        //check if q and list are null
+        if (q == null)
+        {
+            q = new(); list = new();
+            q.Enqueue(this.root);
+        }
+
+        //if q is empty, then we are done - return list
+        if (q.Count == 0) { return list; }
+
+        var current = q.Dequeue();
+
+        //add the current element being inspected to the list
+        list.Add(current);
+
+        //check for left
+        if (current.left != null) { q.Enqueue(current.left); }
+        //check for right
+        if (current.right != null) { q.Enqueue(current.right); }
+
+        //return with a recursive call to BFS_Recursive
+        return BFS_Recursive(q, list);
+    }
+
+
+    public List<Node> BFS_Recursive( Queue<Node> q = null, List<Node> list = null )
+    {
+        if (q == null) //house keeping and the first call
+        { 
+            q = new Queue<Node>(); 
+            list = new List<Node>(); 
+            q.Enqueue(root); 
+        }
+
+        if (q.Count == 0) { return list; }
+
+        Node current = q.Dequeue();
+        list.Add(current);
+        if (current.left != null) { q.Enqueue(current.left); }
+        if (current.right != null) { q.Enqueue(current.right); }
+
+        return BFS_Recursive(q, list);
+
+    }
+
+    public List<Node> BreadthFirstSearch()
+    {
+        Node current = this.root;
+        List<Node> list = new ();
+        Queue<Node> queue = new ();
+
+        //suppose we have the following:
+        //     9
+        //  4     20
+        //1  6  15  170
+
+        // root is 9, and it's assigned to current, we then enque node '9' as this is the starting point
+        // of this algorithm.
+        // We then will perform a loop checking whether the queue is ever empty, if not, pop an element from the
+        //  queue, add it to the return list, and then investigate whether this element has left or right children
+        //  if they exist add them to the queue, and then loop.
+        // The expected outcome should be breadth first so: 9 , 4 , 20 , 1 , 6 , 15 , 170
+
+        queue.Enqueue(current);
+        while (queue.Count > 0)
+        {
+            current = queue.Dequeue();
+            list.Add(current);
+
+            if (current.left != null) 
+            {
+                queue.Enqueue(current.left);
+            }
+            if (current.right != null)
+            {
+                queue.Enqueue(current.right);
+            }
+        }
+
+        return list;
+    }
+
+    public void PrintList(List<Node> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            var item = list[i];
+            Console.WriteLine($"List[{i}] : {item.nValue}");
+
+        }
+    }
+
+
+    //---------------------------------------
+
+    public void Insert(int value)
+    {
+        var newNode = new Node();
+        newNode.nValue = value;
+        //-----
+        if (root == null) { root = newNode; return; }
+
+        var current = root;
+        //-----
+        while (true)
+        {
+            if (value < current.nValue) //left
+            {
+                if (current.left == null)
+                {
+                    current.left = newNode;
+                    return;
+                }
+                current = current.left;
+
+            }
+            else //right
+            {
+                if (current.right == null)
+                {
+                    current.right = newNode;
+                    return;
+                }
+                current = current.right;
+            }
+
+        }
+
+
+    }
+
+    public int? LookUp(int value)
+    {
+        if (root == null) return null;
+
+        var current = root;
+        while (current != null)
+        {
+            if (value == current.nValue) { return current.nValue; }
+            else if (value < current.nValue) //left
+            {
+                current = current.left;
+            }
+            else  //right
+            {
+                current = current.right;
+            }
+        }
+
+        return null;
+    }
+
+    public void Remove(int value)
+    {
+        if (root == null) { return; }
+
+        Node current = root;
+        Node parent = null;
+
+        while (current != null)
+        {
+            if (value < current.nValue) //value < node value -> left
+            {
+                parent = current;
+                current = current.left;
+            }
+            else if (value > current.nValue) //value > node value -> right
+            {
+                parent = current;
+                current = current.right;
+            }
+            else if (value == current.nValue) //value == node value -> do some work
+            {
+                //General rule: we want to delete current, since it matched the value
+                // to be deleted, now we have to manage its possible children
+
+                //by deleting current, we have 2 lose ends that we need to attach somewhere
+                // the general approach is try to work our way around the right node (we will not balance the tree)
+
+                //1 - No right child , then we will connect the parent node to current's left node
+                //       (parent)
+                //           |
+                //       (current)
+                //       /       \
+                // Left(???)    Right(null)
+                if (current.right == null)
+                {
+                    //special case, when there's no parent root will assume the default node position
+                    if (parent == null) { this.root = current.left; }
+                    else
+                    {
+                        //at this point we have the current node and the parent node, and we need to know
+                        // if the current node  is at the left or right of the parent's node so we can replace it
+                        // accordingly
+
+                        //so.. if parent > current value, make current left child a child of its parent
+                        if (current.nValue < parent.nValue) //current sits at the left side
+                        {
+                            parent.left = current.left; //this effectively replaces current node
+                        }
+                        else if (current.nValue > parent.nValue) //current sits at the right side of its parent
+                        {
+                            parent.right = current.left; //this effectively replaces current node
+                        }
+                    }
+                } // 1 end
+
+
+                //2 - There is a right child, and this right child does not have a left child
+                //     then we want to get current.left and place it on current.right.left
+                //     and then since we want to delete current, we will replace the origin link
+                //     from parent, with current.right
+                //
+                //       (parent)
+                //           |
+                //       (current)
+                //       /       \
+                // Left(???)    Right(NOT NULL)
+                //                /       \
+                //             L(null)    R(? maybe not null ?) <- connect this
+                else if (current.right.left == null)
+                {
+                    current.right.left = current.left;
+
+                    //special case, when there's no parent root will assume the default node position
+                    if (parent == null)
+                    {
+                        this.root = current.right;
+                    }
+                    else
+                    {
+                        //at this point we have the current node and the parent node, and we need to know
+                        // if the current node  is at the left or right of the parent's node so we can replace it
+                        // accordingly
+
+                        if (current.nValue < parent.nValue)//current sits at the left side
+                        {
+                            parent.left = current.right; //this effectively replaces current node
+                        }
+                        else if (current.nValue > parent.nValue)  //current sits at the right side of its parent
+                        {
+                            parent.right = current.right; //this effectively replaces current node
+                        }
+                    }
+                } //2 end
+
+
+
+                //3 - There is a right child, and this child has a left child
+                //       (parent)
+                //           |
+                //       (current)
+                //       /       \
+                // Left(???)    Right(NOT NULL)   --------> leftMostParent
+                //              /        \
+                //       L(not null)     R(???)
+                //            |
+                //            |---------------------------> leftMost
+                else
+                {
+                    //Right child -> find its left most child
+                    var leftMost = current.right.left;
+                    var leftMostParent = current.right;
+
+                    while (leftMost.left != null) //trying to get all the way to the left
+                    {
+                        leftMostParent = leftMost;
+                        leftMost = leftMost.left;
+                    }
+
+                    //by now we should be all the way to the left
+
+                    leftMostParent.left = leftMost.right;
+                    leftMost.left = current.left;
+                    leftMost.right = current.right;
+
+
+                    // at this point we reassigned the lose ends,
+                    // we need now to remove 'current' from the tree
+
+                    //------
+
+
+                    //this is a complicated operation but basically we are going to replace the
+                    // current node by the left most node
+                    // everything remains the same but the leftMost.right now is leftMostParent.left
+
+                    if (parent == null)
+                    {
+                        this.root = leftMost;
+                    }
+                    else
+                    {
+
+                        if (current.nValue < parent.nValue)
+                        {
+                            parent.left = leftMost;
+                        }
+                        else if (current.nValue > parent.nValue)
+                        {
+                            parent.right = leftMost;
+                        }
+                    }
+
+
+
+                } //3 end
+
+                return;
+            } // do work end
+
+        } //while end
+
+
+
+        return;
+    }
+
+    public void PrintTree()
+    {
+        Console.WriteLine($"--------------------------\nPrint Tree\n");
+        PrintTree(root, null);
+    }
+
+    private void PrintTree(Node node, Node parentNode)
+    {
+        Console.WriteLine($"    Node Value : {node.nValue}        Parent Node : { parentNode?.nValue}");
+
+        if (node.left != null)
+        {
+            Console.WriteLine($"    Left node below");
+            PrintTree(node.left, node);
+        }
+        if (node.right != null)
+        {
+            Console.WriteLine($"    Right node below");
+            PrintTree(node.right, node);
+        }
+    }
+}
+
+
+
+public class Node
+{
+    public int? nValue = null;
+    public Node left = null;
+    public Node right = null;
+}
