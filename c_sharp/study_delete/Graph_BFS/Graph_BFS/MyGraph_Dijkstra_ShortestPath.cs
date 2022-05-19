7d31eed767cc3123b0e6d86c325f38dcbbaf21ac
Paulo Costa <paulotcj@gmail.com>
2022-05-19 00:56:14 -0400

    .

--- c_sharp/study_delete/Graph_BFS/Graph_BFS/MyGraph_Dijkstra_ShortestPath.cs
+++ c_sharp/study_delete/Graph_BFS/Graph_BFS/MyGraph_Dijkstra_ShortestPath.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Graph_BFS
+﻿namespace Graph_BFS
 {
 
     // Algorithm:
@@ -26,6 +20,7 @@ namespace Graph_BFS
     {
         public static void Test()
         {
+
             var myGraph2 = new MyGraph_Dijkstra_ShortestPath();
 
             //---------------------------------------
@@ -60,12 +55,89 @@ namespace Graph_BFS
             Console.WriteLine($"##############################");
             Console.WriteLine($"Dijkstra Shortest Path - System Identified solution");
             myGraph2.DijkstraPrintSolution("C");
+
+            myGraph2 = null;
+
+            Console.WriteLine($"\n\n##############################");
+            Console.WriteLine($"##");
+            Console.WriteLine($"## NEW TEST");
+            Console.WriteLine($"##");
+            Console.WriteLine($"##############################");
+            Console.WriteLine($"\n\nMatrix input");
+            //possible test:
+            //
+            //     A  B  C  D  E
+            //  A  1  1  0  1  0        
+            //  B  1  1  1  1  1    
+            //  C  0  1  1  0  1    
+            //  D  1  1  0  1  1    
+            //  E  0  1  1  1  1    
+            //
+
+
+            var nodesArray = new string[] { "A", "B", "C", "D", "E" };
+            var edgesArray = new int[][] 
+            {
+                new int[]{1,  1,  0,  1,  0},
+                new int[]{1,  1,  1,  1,  1},
+                new int[]{0,  1,  1,  0,  1},
+                new int[]{1,  1,  0,  1,  1},
+                new int[]{0,  1,  1,  1,  1}
+            };
+            var distanceArray = new int[][]
+            {
+                //          A   B   C   D   E
+                new int[]{  0,  6, -1,  1, -1 }, //A
+                new int[]{  6,  0,  5,  2,  2 }, //B
+                new int[]{ -1,  5,  0, -1,  5 }, //C
+                new int[]{  1,  2, -1,  0,  1 }, //D
+                new int[]{ -1,  2,  5,  1,  0 }  //E
+            };
+
+
+            var myGraph3 = new MyGraph_Dijkstra_ShortestPath();
+            myGraph3.InputDataFromMatrix(headers: nodesArray, rows: edgesArray, distances: distanceArray);
+            myGraph3.Print();
+            Console.WriteLine($"##############################");
+            Console.WriteLine($"Dijkstra Shortest Path");
+            myGraph3.Dijkstra(start: "A");
+
+            myGraph3.PrintDistanceTable();
+            Console.WriteLine($"##############################");
+            Console.WriteLine($"Dijkstra Shortest Path - System Identified solution");
+            myGraph3.DijkstraPrintSolution("C");
+
         }
     }
 
 
     public class MyGraph_Dijkstra_ShortestPath
     {
+        public void InputDataFromMatrix(string[] headers, int[][] rows, int[][] distances)
+        {
+            
+            for(int i = 0; i< headers.Length; i++)
+            {
+                string node1 = headers[i];
+
+                for(int j = 0; j < rows[i].Length; j++)
+                {
+                    string node2;
+                    int distance;
+                    if (rows[i][j] == 1)
+                    {
+                        node2 = headers[j];
+                        distance = distances[i][j];
+                        AddEdge(node1, node2, distance);
+                    }
+                }
+
+            }
+
+
+
+        }
+
         #region distanceTable_comments
         // This table (distanceTable) should work like this:
         //  
@@ -85,7 +157,8 @@ namespace Graph_BFS
         private Dictionary<string, List<NodeDist>> data = new();
         private string Start;
 
-        Queue<string> q = new();
+        Queue<string> q = new(); 
+        //LinkedList<string> q = new();
 
         #region Dijkstra_Algorithm
         public void DijkstraPrintSolution(string endPoint)
@@ -117,11 +190,14 @@ namespace Graph_BFS
             this.Start = start;
             string current;
             q.Enqueue(start);
+            //q.AddLast(start);
             distanceTable.Add(start, new ShortestDistCls(dist: 0, prev: start));
 
             while (q.Count > 0) //standard BFS approach, queue the first node and then reach for all its children and then explore from there
             {
                 current = q.Dequeue();
+                //current = q.First();
+                //q.RemoveFirst();
                 CalculateDistanceTable(current); //this step is far too complicated so it is better to place it in a separate place
             }
 
@@ -134,6 +210,19 @@ namespace Graph_BFS
             List<NodeDist> currentNodeConnections = data[currentNode]; //get a list with all connections for the node which we are accessing
             //---
 
+            Console.WriteLine($"node being investigated {currentNode}");
+            //debug
+            if (currentNode.CompareTo("C") == 0) 
+            { 
+            }
+
+            if (currentNode.CompareTo("B") == 0)
+            {
+            }
+
+            if (currentNode.CompareTo("E") == 0)
+            {
+            }
 
             //---
             // Standard BFS, we will look at every child of the current node
@@ -141,6 +230,13 @@ namespace Graph_BFS
             for (int i = 0; i < currentNodeConnections.Count; i++)
             {
                 NodeDist child = currentNodeConnections[i];
+
+                if(child.Node.CompareTo("E") == 0)
+                {
+                }
+                if (child.Node.CompareTo("C") == 0)
+                {
+                }
 
                 //the options below are simple, we need to identify:
                 // 1 - is there a previous path?
@@ -163,6 +259,11 @@ namespace Graph_BFS
                     {
                         currentNodeDistance.ShortestDistance = sumDistances; //updated new shorter distance
                         currentNodeDistance.Prev = child.Node; //the shortest path now is taken through the current child node
+
+                        //if the shortest distance of this path is altered we need to reevaluate all paths connected to this path
+                        // therefore, we need to enqueue again
+                        q.Enqueue(currentNode);
+
                     }
 
                 }
@@ -173,6 +274,8 @@ namespace Graph_BFS
                     int sumDistances = child.EdgeDistance + currentNodeDistance.ShortestDistance;
                     distanceTable.Add(child.Node, new ShortestDistCls(dist: sumDistances, prev: currentNode));
                     q.Enqueue(child.Node);
+                    //q.AddLast(child.Node);
+
                 }
 
 
@@ -195,10 +298,15 @@ namespace Graph_BFS
 
         public void AddEdge(string node1, string node2, int dist)
         {
+            if (node1.CompareTo(node2) == 0) { return; }
             AddVertex(node1);
             AddVertex(node2);
-            data[node1].Add(new NodeDist(node2, dist));
-            data[node2].Add(new NodeDist(node1, dist));
+            bool entryExists;
+            entryExists = data[node1].Any(x => x.Node.CompareTo(node2) == 0);
+            if (entryExists == false) { data[node1].Add(new NodeDist(node2, dist)); }
+            entryExists = data[node2].Any(x => x.Node.CompareTo(node1) == 0);
+            if (entryExists == false) { data[node2].Add(new NodeDist(node1, dist)); }
+            
         }
         #endregion
 
@@ -222,7 +330,7 @@ namespace Graph_BFS
             foreach (var tempNode in tempData)
             {
                 var sb = new System.Text.StringBuilder();
-                foreach (var edge in tempNode.Value)
+                foreach (var edge in tempNode.Value.OrderBy(x=> x.Node).ToList())
                 {
                     sb.Append($"{edge.Node} (dist:{edge.EdgeDistance}), ");
                 }
